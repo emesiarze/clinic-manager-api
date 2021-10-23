161aee85c42d67eb5a3c03ea7e5d806e651d6655
Hubert Wawrzacz <hwawrzacz.dev@gmail.com>
2021-10-23 15:00:29 +0200

    Add generic CommonController

---
+++ Controllers/CommonController.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using CinemaManagerApi.Models;
+using CinemaManagerApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaManagerApi.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class CommonController<T> : ControllerBase
+  {
+    private readonly IRepository<T> repository;
+
+    public CommonController(IRepository<T> repository)
+    {
+      this.repository = repository;
+    }
+
+    [HttpGet]
+    public IEnumerable<T> GetAllItems()
+    {
+      var items = repository.GetAllItems();
+      return items;
+    }
+
+    [HttpGet("{id}")]
+    public T GetItem(int id)
+    {
+      var item = repository.GetSingleItem(id);
+      return item;
+    }
+
+    [HttpPost]
+    public string AddItem(T item)
+    {
+      this.repository.AddItem(item);
+      return "success";
+    }
+
+    [HttpPut("{id}")]
+    public string UpdateItem(int id, T item)
+    {
+      this.repository.UpdateItem(item);
+      return "success";
+    }
+
+    [HttpDelete("{id}")]
+    public string DeleteItem(int id)
+    {
+      this.repository.DeleteItem(id);
+      return "success";
+    }
+  }
+}
--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -5,50 +5,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CinemaManagerApi.Controllers
 {
-  [ApiController]
-  [Route("users")]
-  public class UsersController : ControllerBase
+  public class UsersController : CommonController<User>
   {
     private readonly IRepository<User> repository;
 
-    public UsersController(IRepository<User> repository)
+    public UsersController(IRepository<User> repository) : base(repository)
     {
       this.repository = repository;
-    }
-
-    [HttpGet]
-    public IEnumerable<User> GetAllItems()
-    {
-      var users = repository.GetAllItems();
-      return users;
-    }
-
-    [HttpGet("{id}")]
-    public User GetItem(int id)
-    {
-      var user = repository.GetSingleItem(id);
-      return user;
-    }
-
-    [HttpPost]
-    public string AddItem(User user)
-    {
-      this.repository.AddItem(user);
-      return "success";
-    }
-
-    [HttpPut("{id}")]
-    public string UpdateItem(int id, User user)
-    {
-      this.repository.UpdateItem(user);
-      return "success";
-    }
-
-    [HttpDelete("{id}")]
-    public string DeleteItem(int id)
-    {
-      this.repository.DeleteItem(id);
-      return "success";
     }
   }
 }
