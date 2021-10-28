ec25bc9c8288554168656121cfd83fb1a5453d3b
Hubert Wawrzacz <hwawrzacz.dev@gmail.com>
2021-10-28 20:49:36 +0200

    Add admin user

--- Data/UsersData.cs
+++ Data/UsersData.cs
@@ -6,10 +6,10 @@ namespace CinemaManagerApi.Data
   public static class UsersData
   {
     public static List<User> items = new List<User>() {
-    new User("Jan Kowalski", "jankow", "qwer", false),
-    new User("Tomasz Malinowski", "tommal", "qwer", false),
-    new User("Jerzy Gwóźdź", "jegwoz", "qwer", true)
-    // { id: 4, fullName: "Nick Adams", login: "nicada", password: "qwer", isWorker: false }    
+    new User("Admin", "asdf", "asdf", true),
+    new User("Jan Kowalski", "jankow", "asdf", false),
+    new User("Adam Nowak", "adanow", "asdf", false),
+    new User("Jerzy Gwóźdź", "jergwo", "asdf", false)
   };
   }
 }
